a11711fd0e1f5a75d44d68469001d5164167ecb7
KhangHuynh <khanghqse182958@fpt.edu.vn>
2025-10-28 13:54:27 +0700

    refactor: import ProductApi

--- app/src/main/java/com/example/prm392_android_app_frontend/data/remote/api/ProductApi.java
+++ app/src/main/java/com/example/prm392_android_app_frontend/data/remote/api/ProductApi.java
@@ -6,9 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.QueryMap;
 
 public interface ProductApi {
