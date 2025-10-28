9dcfab26abcdd9eabd14072ce9985de629823f47
KhangHuynh <khanghqse182958@fpt.edu.vn>
2025-10-28 15:25:02 +0700

    fix: haha

--- app/src/main/java/com/example/prm392_android_app_frontend/data/remote/api/ApiClient.java
+++ app/src/main/java/com/example/prm392_android_app_frontend/data/remote/api/ApiClient.java
@@ -17,8 +17,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public final class ApiClient {
-//    public static String BASE_URL = "https://plowable-nonlevel-sharie.ngrok-free.dev/api/";
-    public static String BASE_URL= "http://10.0.2.2:8080/api/";
+    public static String BASE_URL = "https://plowable-nonlevel-sharie.ngrok-free.dev/api/";
+//    public static String BASE_URL= "http://10.0.2.2:8080/api/";
 //    public static String BASE_URL= "http://localhost:8080/api/";
     private static volatile Retrofit retrofit;
     private static volatile Retrofit retrofitAuth;
