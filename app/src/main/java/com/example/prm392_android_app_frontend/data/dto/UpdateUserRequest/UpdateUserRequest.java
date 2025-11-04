ec46c1402b91799b6c715f82059de3c50ca31a86
PhuItsme <tvtan1977@gmail.com>
2025-11-04 11:28:50 +0700

    Update add address
    Manual for district , ward , province ( Nhập tay)

--- app/src/main/java/com/example/prm392_android_app_frontend/data/dto/UpdateUserRequest/UpdateUserRequest.java
+++ app/src/main/java/com/example/prm392_android_app_frontend/data/dto/UpdateUserRequest/UpdateUserRequest.java
@@ -3,10 +3,10 @@ package com.example.prm392_android_app_frontend.data.dto.UpdateUserRequest;
 public class UpdateUserRequest  {
     public String username;
     public String email;
-    public String password;     // để null nếu không đổi
+    public String password;
+    public String oldPassword;
     public String phoneNumber;
-    public String role;         // nếu backend bắt buộc, điền lại giá trị cũ
-
+    public String role;
     public void setRole(String role) {
         this.role = "CUSTOMER";
     }
